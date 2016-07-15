135b9aa5ac53e2739c8494ac31af3a6a679b5346
Nakul Pathak <nakulpathak3@hotmail.com>
2016-07-15 17:58:52 -0400

    Remove GUI and merge deck_window code to view

--- view.cc
+++ view.cc
@@ -1,15 +1,4 @@
-/*
- * MVC example of GTKmm program
- *
- * View class.  Is responsible for buttons (that user clicks) and for displaying
- * the top card of the deck.
- *
- *  Created by Jo Atlee on 06/07/09.
- *  Copyright 2009 UW. All rights reserved.
- *
- */
-
-
+/* Thanks Jo Atlee */
 #include "observer.h"
 #include "view.h"
 #include "controller.h"
@@ -17,35 +6,114 @@
 #include "subject.h"
 #include "DeckGUI.h"
 #include <iostream>
+#include <sstream>
 
-// Creates buttons with labels. Sets butBox elements to have the same size, 
+// Creates buttons with labels. Sets butBox elements to have the same size,
 // with 10 pixels between widgets
-View::View(Controller *c, Model *m) : model_(m), controller_(c), panels(true,10), butBox(true, 10), next_button( "next" ), reset_button( "reset" ), card(deck.null()) {
+View::View(Controller *c, Model *m) : model_(m), controller_(c), playedLabel("Played cards:"),
+                                      hboxHand( true, 10 ), control_panel( true, 10 ),
+                                      new_game( "New Game" ), quit_game( "Quit" ),
+                                      nameLabel( "Enter seed:" ), handLabel( "Current Hand:" ) {
 
-	// Sets some properties of the window.
-        set_title( "CS247 MVC example" );
-	set_border_width( 10 );
-	
-	// Add panels to the window
-	add(panels);
+    nextCard = 0;
+    nextSuit = (Suits) 0;
+    nextFace = (Faces) 0;
 
-	// Add button box and card image to the panels
-	panels.add( butBox );
-	panels.add( card );
-	card.set( deck.null() );
+    const Glib::RefPtr<Gdk::Pixbuf> nullCardPixbuf = deck.null();
+    const Glib::RefPtr<Gdk::Pixbuf> cardPixbuf     = deck.image( ACE, SPADE );
 
-	// Add buttons to the box (a container). Buttons initially invisible
-	butBox.add( next_button );
-	butBox.add( reset_button );
+    // Sets the border width of the window.
+    set_border_width( 10 );
 
+    // Set the look of the frame.
+    frame.set_label( "Straights!" );
+    frame.set_label_align( Gtk::ALIGN_CENTER, Gtk::ALIGN_TOP );
+    frame.set_shadow_type( Gtk::SHADOW_ETCHED_OUT );
 
+    // Add the frame to the window. Windows can only hold one widget, same for frames.
+    add( frame );
+    frame.add(vbox);
+
+    //Add game controls
+    vbox.add(control_panel);
+    control_panel.add(new_game);
+    new_game.signal_clicked().connect(sigc::mem_fun( *this, &View::onNewGame));
+    control_panel.pack_start( nameLabel );
+    control_panel.pack_start( nameField );
+    nameField.set_text( "0" );
+    control_panel.add(quit_game);
+    quit_game.signal_clicked().connect(sigc::mem_fun( *this, &View::onQuitGame));
+
+//Adding the played cards section -----------------------------------
+    vbox.add(playedLabel);
+    // Add the horizontal box for laying out the images to the frame.
+
+    for (int i=0;i<4;i++){
+        hboxCards[i] = Gtk::manage(new Gtk::HBox());
+        vbox.add( *hboxCards[i] );
+        // Initialize 4 empty cards and place them in the box.
+        for (int j = 0+13*i; j < 13*(i+1); j++ ) {
+            card[j] = new Gtk::Image( nullCardPixbuf );
+            hboxCards[i]->add( *card[j] );
+        }
+    }
+
+    std::cout<<"Finished loop"<<std::endl;
+    // Initialize the 5th card and place the image into the button.
+    //  card[4] = new Gtk::Image( cardPixbuf );
+    // button.set_image( *card[12] );
+    std::cout<<"Finished setting card"<<std::endl;
+
+    // Attach event listener to button
+    // button.signal_clicked().connect(sigc::mem_fun( *this, &View::onButtonClicked));
+
+    // Add the button to the box.
+    // hboxClubs.add( button );
+// Adding player boxes
+    player_list = Gtk::manage(new Gtk::HBox());
+    vbox.pack_start( *player_list );
+
+    for (int i=0;i<4;i++){
+
+        player_modules[i] = Gtk::manage(new Gtk::VBox());
+        player_list->pack_start(*player_modules[i]);
+
+        std::stringstream ss;
+        ss << i+1;
+        pLabels[i] = new Gtk::Label("Player " + ss.str());
+        player_modules[i]->pack_start(*pLabels[i]);
+
+        scoreLabels[i] = new Gtk::Label("Score: ");
+        player_modules[i]->pack_start(*scoreLabels[i]);
+
+        ragequitButtons[i] = new Gtk::Button("Rage");
+        ragequitButtons[i]->signal_clicked().connect( sigc::bind<int>( sigc::mem_fun(*this, &View::onRagequit), i) );
+        player_modules[i]->pack_start(*ragequitButtons[i]);
+    }
+
+    std::cout<<"Tried to make player module"<<std::endl;
+
+// Adding the hand
+    vbox.add( handLabel);
+    vbox.add( hboxHand );
+    // Initialize 13 empty cards and place them in the box.
+    for (int i = 0; i < 13; i++ ) {
+        handButtons[i] = new Gtk::Button();
+        // Glib::RefPtr<Gdk::Pixbuf> cardImg = deck.image( static_cast<Faces>(i), SPADE );
+        // handButtons[i]->set_image(*(new Gtk::Image(cardImg)));
+        handButtons[i]->set_image(*(new Gtk::Image(nullCardPixbuf)));
+        // card[i] = new Gtk::Image( nullCardPixbuf );
+        hboxHand.add(*handButtons[i]);
+        // handButtons[i]->signal_clicked().connect(sigc::mem_fun( *this, &View::onButtonClicked));
+        handButtons[i]->signal_clicked().connect( sigc::bind<int>( sigc::mem_fun(*this, &View::onCardClicked), i) );
+        // handButtons[i]->signal_clicked().connect( sigc::bind<Glib::ustring>( sigc::mem_fun(*this, &View::onCardClicked), "button 1") );
+    }
+
+    // The final step is to display this newly created widget.
+    show_all();
 	// Associate button "clicked" events with local onButtonClicked() method defined below.
-	next_button.signal_clicked().connect( sigc::mem_fun( *this, &View::nextButtonClicked ) );
-	reset_button.signal_clicked().connect( sigc::mem_fun( *this, &View::resetButtonClicked ) );
-	
-	
-	// The final step is to display the buttons (they display themselves)
-	show_all();
+	// next_button.signal_clicked().connect( sigc::mem_fun( *this, &View::nextButtonClicked ) );
+	// reset_button.signal_clicked().connect( sigc::mem_fun( *this, &View::resetButtonClicked ) );
 
 	// Register view as observer of model
 	model_->subscribe(this);
@@ -54,15 +122,46 @@ View::View(Controller *c, Model *m) : model_(m), controller_(c), panels(true,10)
 
 View::~View() {}
 
-
 void View::update() {
   Suits suit = model_->suit();
   Faces face = model_->face();
-  if ( suit == NOSUIT ) 
-    card.set( deck.null() );
-  else
-    card.set( deck.image(face, suit) );
+  // if ( suit == NOSUIT )
+  //   card.set( deck.null() );
+  // else
+  //   card.set( deck.image(face, suit) );
+}
 
+void View::onNewGame(){
+    std::cout<<"New Game. Seed = "<< nameField.get_text() <<std::endl;
+}
+
+void View::onQuitGame(){
+    std::cout<<"Quit Game."<<std::endl;
+}
+
+void View::onCardClicked(int i){
+    std::cout<<"Card "<<i<< " clicked."<<std::endl;
+}
+
+void View::onRagequit(int i){
+    std::cout<<"Player "<<i+1<< " ragequit."<<std::endl;
+}
+
+void View::onButtonClicked()
+{
+    card[nextCard]->set(deck.image(nextFace, nextSuit));
+
+    nextCard = (nextCard+1) % 13;
+    if( (nextFace+1) == 13)
+    {
+        nextFace = (Faces) 0;
+        nextSuit = (Suits) ((nextSuit + 1) % 4);
+    }
+    else
+    {
+        nextFace = (Faces) (nextFace+1);
+    }
+    std::cout<< "Button clicked"<<std::endl;
 }
 
 void View::nextButtonClicked() {
--- view.h
+++ view.h
@@ -14,6 +14,13 @@
 #define MVC_VIEW_H
 
 #include <gtkmm.h>
+#include <gtkmm/window.h>
+#include <gtkmm/image.h>
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/frame.h>
+#include <gtkmm/entry.h>
+
 #include "DeckGUI.h"
 #include "observer.h"
 
@@ -23,27 +30,57 @@ class Model;
 
 class View : public Gtk::Window, public Observer {
 public:
-        View( Controller*, Model* );
+    View( Controller*, Model* );
+    void onButtonClicked();
+    void onNewGame();
+    void onCardClicked(int);
+    void onRagequit(int);
+    void onQuitGame();
 	virtual ~View();
 	virtual void update();	// Observer Pattern: concrete update() method
 
 private:
 	// Observer Pattern: to access Model accessors without having to downcast subject
 	Model *model_;
-	
+
 	// Strategy Pattern member (plus signal handlers)
 	Controller *controller_;
 
 	// Card Images
-	DeckGUI deck;
+	DeckGUI deck;    // Knows all of the card pixel buffers.
 
 	// Member widgets:
-	Gtk::HBox panels;      // Main window divided into two horizontal panels
-	Gtk::VBox butBox;      // Vertical boxes for stacking buttons vertically
-	Gtk::Button next_button;
-	Gtk::Button reset_button;
-	Gtk::Image card;
+	// Gtk::HBox panels;      // Main window divided into two horizontal panels
+	// Gtk::VBox butBox;      // Vertical boxes for stacking buttons vertically
+	// Gtk::Button next_button;
+	// Gtk::Button reset_button;
+	// Gtk::Image card;
 
+    int nextCard;
+    Suits nextSuit;
+    Faces nextFace;
+
+    // Member widgets:
+    Gtk::Image * card[52];          // Images to display.
+    Gtk::Image * hand[13];          // Images to display.
+    // Gtk::Button button;           // Button that will hold an image.
+    Gtk::Button * handButtons[13];
+    Gtk::HBox * hboxCards[4];
+    Gtk::HBox hboxHand;
+    Gtk::VBox vbox;
+    Gtk::HBox control_panel;
+    Gtk::HBox* player_list;
+    Gtk::VBox* player_modules[4];
+    Gtk::Entry nameField; // Text entry for the user's name
+    Gtk::Button new_game;
+    Gtk::Button quit_game;
+    Gtk::Button * ragequitButtons[4];
+    Gtk::Label playedLabel;
+    Gtk::Label handLabel;
+    Gtk::Label nameLabel;
+    Gtk::Label * scoreLabels[4];
+    Gtk::Label * pLabels[4];
+    Gtk::Frame frame;
 	// Signal handlers:
 	void nextButtonClicked();
 	void resetButtonClicked();
