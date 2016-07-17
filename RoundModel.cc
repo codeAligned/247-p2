9b6d45509f2e70d60fd71eb5f6d4cb08854e3cc3
Nakul Pathak <nakulpathak3@hotmail.com>
2016-07-16 23:23:24 -0400

    Forgot to shuffle

--- RoundModel.cc
+++ RoundModel.cc
@@ -7,6 +7,7 @@ using namespace std;
 
 RoundModel::RoundModel(int seed) : topCard_(-1) {
   deck_ = new Deck(seed);
+  deck_->shuffle();
 }
 
 RoundModel::RoundModel(std::vector<Player*> players, Deck* new_deck) {
